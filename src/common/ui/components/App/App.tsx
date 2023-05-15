dc6334ffe7f8f93db4cdd7c278a402b5d1a29d6b
ValentainFomkin <valentain1625@mail.ru>
2023-05-15 20:09:44 +0300

    styles

--- src/common/ui/components/App/App.tsx
+++ src/common/ui/components/App/App.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import s from 'common/ui/components/App/App.module.css'
 import {Boubles} from "common/ui/components/Boubles/Boubles";
 import {Wrapper} from "common/ui/components/Wrapper/Wrapper";
+import {HashRouter} from "react-router-dom";
 
 
 export const App = () => {
    return (
       <div className={s.app}>
-         <span className={s.redBuble}></span>
-         {/*<header className={s.header}>header</header>*/}
-         {/*<main className={s.main}>main</main>*/}
-         {/*<footer className={s.footer}>footer</footer>*/}
-         <Boubles/>
-         <Wrapper/>
+         <HashRouter>
+            <span className={s.redBuble}></span>
+            {/*<header className={s.header}>header</header>*/}
+            {/*<main className={s.main}>main</main>*/}
+            {/*<footer className={s.footer}>footer</footer>*/}
+            <Boubles/>
+            <Wrapper/>
+         </HashRouter>
       </div>
    );
 }
-
